c3cf8e594ae383b711e58093b785f88c261cab07
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-03-15 19:27:28 -0500

    Part4 index page now functional

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -33,8 +33,8 @@ public class HomeController {
 
     @RequestMapping("")
     public String index(Model model) {
-
         model.addAttribute("title", "My Jobs");
+        model.addAttribute("jobs", jobRepository.findAll());
 
         return "index";
     }
